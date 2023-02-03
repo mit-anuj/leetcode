972ab01ee688cec3ab9e4764b1e45464e0945da7
mit-anuj <93519859+mit-anuj@users.noreply.github.com>
2023-02-03 23:31:49 +0530

    delete middle node of a linked list

---
+++ deleteMiddle.java
@@ -0,0 +1,57 @@
+// You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+// The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+// For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+
+
+// Input: head = [1,3,4,7,1,2,6]
+// Output: [1,3,4,1,2,6]
+// Explanation:
+// The above figure represents the given linked list. The indices of the nodes are written below.
+// Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+// We return the new list after removing this node. 
+
+// Input: head = [1,2,3,4]
+// Output: [1,2,4]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+
+
+// Input: head = [2,1]
+// Output: [2]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+// Node 0 with value 2 is the only node remaining after removing node 1.
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 105].
+// 1 <= Node.val <= 105
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+
+         // Edge case: return nullptr if there is only one node.
+        if (head.next == null)
+            return null;
+        
+        // Initialize two pointers, 'slow' and 'fast'.
+        ListNode slow = head, fast = head.next.next;
+        
+        // Let 'fast' move forward by 2 nodes, 'slow' move forward by 1 node each step.
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // When 'fast' reaches the end, remove the next node of 'slow' and return 'head'.
+        slow.next = slow.next.next;
+        return head;
+
+    }
+}
